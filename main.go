8717e2fc41bef57df192d542d90173aad36f7050
fazilnbr <fazilkp2000@gmail.com>
2023-05-11 14:56:43 +0530

    feat():add basic scraping
    here i done the basic scraping using colly package and i scrap random quotes and auther from site and print it

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
+func main() {
+	c := colly.NewCollector(
+		colly.AllowedDomains("quotes.toscrape.com"),
+	)
+
+	c.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36")
+		fmt.Println("visiting", r.URL)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println("response status code : ", r.StatusCode)
+	})
+
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Println("error : ", err.Error())
+	})
+
+
+	c.OnHTML("span.text",func(h *colly.HTMLElement) {
+		fmt.Println("\n\nQuotes :- ",h.Text)
+	})
+
+	c.OnHTML("small.author",func(h *colly.HTMLElement) {
+		fmt.Println("\n\nAuthor :- ",h.Text)
+	})
+
+	c.Visit("https://quotes.toscrape.com/random")
+
+}
